05587e7af97758d56aa4619692411d72ebc19436
Jake Burdass <jburdass@outlook.com>
2023-02-15 11:23:24 +0000

    project set up - ready to start coding

---
+++ bassment-media-player/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+      <header></header>
+      <div className="App">
+        <h2>HELLO</h2>
+      </div>
+      <Routes>
+
+      </Routes>
+    </>
+  );
+}
+
+export default App;
